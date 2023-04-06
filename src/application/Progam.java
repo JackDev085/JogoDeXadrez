aad83b8f4a900b0b611766640df9cd200246b037
Jackson Lorran <computadordb@gmail.com>
2023-04-06 15:23:04 -0300

    Creat firts class position

--- src/application/Progam.java
+++ src/application/Progam.java
@@ -1,10 +1,12 @@
 package application;
 
+import BoardGame.Position;
+
 public class Progam {
 
 	public static void main(String[] args) {
-		System.out.println("Hello wordl!!");
-
+		Position pos = new Position(1, 3);
+		System.out.println(pos);
 	}
 
 }
